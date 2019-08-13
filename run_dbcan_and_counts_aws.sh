2b02f94ae7e377e5bd605fe768e846dd85e852ec
dadahan <surfingdylan@gmail.com>
2019-08-13 16:03:30 -0700

    building dockerized aws submit script

--- run_dbcan_and_counts_aws.sh
+++ run_dbcan_and_counts_aws.sh
@@ -2,14 +2,13 @@
 # e.g., run with:
 # run_dbcan_and_counts.sh genome.fa
 # genome name is first argument
+export PATH="/opt/conda/bin:${PATH}"
 genome=$1
 # strip everything down to genome name after last backslash and strip .fna extension
 name=`echo $genome | sed 's:.*/::' | sed 's/.fna//'`
 
 # run dbcan on genome
-python ${SCRIPTS_DIR}/run_dbcan.py --cluster --out_dir ${name} --out_pre ${name}_ ${genome} prok
+python /mnt/scripts/run_dbcan.py --cluster --out_dir ${name} --out_pre ${name}_ ${genome} prok
 
 # generate consensus counts by calls
-python ${SCRIPTS_DIR}/generate_dbCAN_consensus_and_counts.py ${name} --out_pre ${name}_ --min_tools 2
-
-
+python /mnt/scripts/generate_dbCAN_consensus_and_counts.py ${name} --out_pre ${name}_ --min_tools 2
